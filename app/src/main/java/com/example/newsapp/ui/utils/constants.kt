c7dc283a09eb1b9a2d2dd6a62f251d8b225379ce
drekza <drekza@yandex.ru>
2021-07-10 18:47:00 +0700

    Set up retrofit

---
+++ app/src/main/java/com/example/newsapp/ui/utils/constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.ui.utils
+
+class constants {
+    companion object{
+        const val API_KEY = "d9de84bd46074e99afb1a0303b1e90b1"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
