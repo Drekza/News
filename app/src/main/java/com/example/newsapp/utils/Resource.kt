c7fb4df5a334b58b6be99b75e067c1fa18c3bce6
drekza <drekza@yandex.ru>
2021-07-10 22:01:00 +0700

    Created Resource class

--- app/src/main/java/com/example/newsapp/utils/Resource.kt
+++ app/src/main/java/com/example/newsapp/utils/Resource.kt
@@ -1,4 +1,12 @@
 package com.example.newsapp.utils
 
-class Resource {
+import retrofit2.Response
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Loading<T> : Resource<T>()
 }
